f24b905f0a0c49f34a98a20a7195ec5cfe2c4090
hchangjae <hcj@payw.org>
2020-12-08 01:07:40 +0900

    Change css reset code to top

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,27 @@ export default function Home() {
         <input className='main-input' />
       </main>
 
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+        
+        input:focus,
+        select:focus,
+        textarea:focus,
+        button:focus {
+            outline: none;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
       <style jsx>{`
         main {
           height:100vh;
@@ -36,35 +57,13 @@ export default function Home() {
           font-size: 1.6rem;
 
           background: #FFFFFF;
-          -webkit-box-shadow: 0px 40px 30px rgba(0, 0, 0, 0.03), 0px 25px 20px rgba(0, 0, 0, 0.05), 0px 10px 15px rgba(0, 0, 0, 0.1), 0px 1px 5px rgba(0, 0, 0, 0.2);
+          -webkit-appearance: none;
           box-shadow: 0px 40px 30px rgba(0, 0, 0, 0.03), 0px 25px 20px rgba(0, 0, 0, 0.05), 0px 10px 15px rgba(0, 0, 0, 0.1), 0px 1px 5px rgba(0, 0, 0, 0.2);
           border-radius: 10px;
           border: none;
         }
 
         
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        
-        input:focus,
-        select:focus,
-        textarea:focus,
-        button:focus {
-            outline: none;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
       `}</style>
     </div>
   )
